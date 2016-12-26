35a1178aa55a965e50b667f99ca3e5b8c515e445
Ada Warren <alnwarr@vt.edu>
2016-12-25 23:41:45 -0500

    Better automation

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+TIMEOUT=20m
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+trap 'kill -INT -$pid; rm data/out$i; echo; exit' INT
+
 for i in `seq 0 99`; do
 	echo "run $i"
-	./net > data/out$i
+
+	timeout $TIMEOUT "$DIR/net" > data/out$i &
+	pid=$!
+	wait $pid
+
+	if [ $? != 0 ]; then
+		echo "Timed out"
+		rm data/out$i
+	fi
 done
 
-python prob.py data/out*
+python "$DIR/prob.py" data/out* > final
