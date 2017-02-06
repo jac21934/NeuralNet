915d2260051b7b989d1c6026d37dd712d809331d
Ada Warren <alnwarr@vt.edu>
2017-02-06 12:56:35 -0500

    misc

--- doNet.cmd
+++ doNet.cmd
@@ -1,8 +1,6 @@
 executable      = net
 universe        = standard
 
-periodic_remove = ((CommittedTime - CommittedSuspensionTime) > 600)
-
 error           = errors/error.$(Process)
 log             = logs/log.$(Process)
 output          = data/out.$(Process)
--- src/noise.h
+++ src/noise.h
@@ -2,10 +2,10 @@
 #define NOISE_H
 
 template<class distribution, class URNG>
-void random_noise(double *neurons, int size, distribution &amplitude, URNG &g) {
+void random_noise(double *neurons, int size, distribution &noise_dist, URNG &g) {
 	std::uniform_int_distribution<int> neuron_dist(0, size - 1);
 
-	neurons[neuron_dist(g)] += amplitude(g);
+	neurons[neuron_dist(g)] += noise_dist(g);
 }
 
 #endif /*NOISE_H*/
