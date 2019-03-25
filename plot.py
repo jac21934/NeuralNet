fed0556001816fb86498c7d5736308c9fb959c2c
Jacob <jac21934@vt.edu>
2019-03-25 16:27:13 -0400

    neuron disabling

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import sys
 import matplotlib
 import matplotlib.pyplot as plt
-
+import numpy as np
 matplotlib.rc('font', size=16)
 
 for file in sys.argv[1:-1]:
@@ -16,14 +16,16 @@ for file in sys.argv[1:-1]:
 		ys.append(float(y))
 		#sigmas.append(float(sigma))
 	data.close()
-
-	try:
-		plt.errorbar(xs, ys, fmt="o", label=file.split('/')[-2])
-	except IndexError:
-		plt.errorbar(xs, ys, fmt="o")
-
+        print xs
+	plt.errorbar(xs, ys, fmt="o" )
+                
 plt.xscale("log")
 plt.yscale("log")
+
+plt.xlabel("$P(S)$", fontsize=20)
+plt.ylabel("$S$", fontsize=20)
+
+plt.tight_layout()
 
 plt.legend()
 
